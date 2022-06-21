d844dae0bb8e33340f77aafb2f2d8ce8f52115c3
uloydev <47286547+uloydev@users.noreply.github.com>
2022-06-22 00:13:41 +0700

    [ADD] logout button on admin pages

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -47,7 +47,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="/" class="brand-link">
                 <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
@@ -94,6 +94,18 @@
                                 <i class="nav-icon fas fa-th"></i>
                                 <p>Gallery Event</p>
                             </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('admin.contact.index') }}" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>Contact</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <form method="POST" class="px-3" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="btn btn-sm btn-block btn-danger mt-5">Logout</button>
+                            </form>
                         </li>
                     </ul>
                 </nav>
