930d2cd1774010ee44a831ab3af44eb38df189e1
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-06-21 16:33:47 +0700

    [UPDATE] redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/admin/profile/1/edit';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
