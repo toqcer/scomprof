0f5ef1686f389bc6c22283c02f4cdf315b666017
fathrahh <gazoon3@gmail.com>
2022-06-22 15:02:14 +0700

    [UPDATE] Icon sidebar & preloader

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -35,7 +35,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__wobble" src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
+            <h1>{{config('app.name')}}</h1>
         </div>
 
         <!-- Navbar -->
@@ -48,19 +48,13 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="/" class="brand-link">
-                <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
-                <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+                <span class="text-center d-block brand-text font-weight-light">{{ config('app.name') }}</span>
             </a>
 
             <!-- Sidebar -->
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" 
-                        alt="User Image">
-                    </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ auth()->user()->username }}</a>
                     </div>
@@ -73,7 +67,7 @@
                         data-accordion="false">
                         <li class="nav-item">
                             <a href="{{ route('admin.profile.edit', 1) }}" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
+                                <i class="nav-icon fa fa-user"></i>
                                 <p>Profile</p>
                             </a>
                         </li>
@@ -85,19 +79,19 @@
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('admin.teacher.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
+                                <i class="nav-icon fas fa-chalkboard-teacher"></i>
                                 <p>Guru</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('admin.gallery.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
+                                <i class="nav-icon fas fa-images"></i>
                                 <p>Gallery Event</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('admin.contact.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
+                                <i class="nav-icon fas fa-phone-alt"></i>
                                 <p>Contact</p>
                             </a>
                         </li>
